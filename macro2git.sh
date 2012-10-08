392d006f92ea241dbbab701aab0d71ce5e9aa1b3
Nicolas Vincent <nvincent@sequans.com>
2012-10-08 12:35:08 +0200

    Improve importation scripts

--- macro2git.sh
+++ macro2git.sh
@@ -36,14 +36,25 @@ retrieve_filelist()
 get_deps()
 {
   local src=${1}
+  local incdirs=${2}
   if [ -f ${src} ]; then
-    local deps=`grep 'include' $src | sed '/\/\//d' | sed 's/.*include\s*"\(.*\)".*/\1/' | sort | uniq`
+    local deps=`grep '^\s*.include' $src | sed '/\/\//d' | sed 's/.*include\s*"\(.*\)".*/\1/' | sort | uniq`
   fi
   if [ -n "$deps" ]; then
     echo "${deps}"
     for d in ${deps}; do
+      # check if included file is directly accessible from where we are
       if [ -f ${d} ]; then
-        get_deps $d
+        get_deps ${d} ${incdirs}
+      else
+        # looks for incdir
+        local deps_inc=""
+        for i in ${INCDIRS}; do
+          deps_inc="${deps_inc} `find ${i} -maxdepth 1 -name ${d}`"
+        done
+        for di in ${deps_inc}; do
+          get_deps ${di} ${incdirs}
+        done
       fi
     done
   fi
@@ -51,9 +62,12 @@ get_deps()
 
 # parse args
 while [ $# -gt 0 ] ; do
-  case $1 in 
+  case $1 in
     -macro)
       MACRO=$2
+      shift;;
+    -top)
+      TOP=$2
       shift;;
     -filelist)
       FILELIST=$2
@@ -63,6 +77,7 @@ while [ $# -gt 0 ] ; do
       shift;;
     -h|*)
       echo "usage : $0 -macro macro_name -filelist /path/to/macro_filelist"
+      echo " -dest /path/to/copy/files/to"
       exit 1
       ;;
   esac
@@ -70,27 +85,33 @@ while [ $# -gt 0 ] ; do
 done
 # default values
 MACRO=${MACRO:-scs_meas}
+TOP=${TOP:-${MACRO}}
 FILELIST=${FILELIST:-./srcv/tops/lte_tops/file_list_scs_meas}
-DEST=${DEST:-${HOME}/work/SQN3210_product}/${MACRO}
+DEST=${DEST:-${HOME}/work/SQN3210_product/importFrom3210}/${MACRO}
 
-#cleanup destination folder if it exists already 
-rm -Rf $DEST
-mkdir -p $DEST
+#cleanup destination folder if it exists already
+rm -Rf ${DEST}
+mkdir -p ${DEST}
 
+# remove ASIC link to avoid ln complains that it exists already
+rm -f ASIC
 ln -s ./ ASIC
 
 # build filelist with filepp
 HDL_FILES=`${FILEPP} -e -kc "//#" ${FILELIST} | sed '/^$/d'`
 
 # sort result of filepp
-# Excludes asiclib as it is handled differently 
+# Excludes asiclib as it is handled differently
 # (one repository for asiclib shared with all macros)
 VHDL_FILES=`echo ${HDL_FILES} | sed 's/ /\n/g' | sort | uniq | grep '.vhdl\?$' | grep -v "asiclib"`
-VERILOG_FILES=`echo ${HDL_FILES} | sed 's/ /\n/g' | sort | uniq | grep '.v$' | grep -v "asiclib"`
+VERILOG_FILES=`echo ${HDL_FILES} | sed 's/ /\n/g' | sort | uniq | grep '.v$' | grep -v "asiclib" | grep -v "memories"`
 INCDIRS=`echo ${HDL_FILES} | sed 's/ /\n/g' | sort | uniq | grep incdir | sed 's/+incdir+//' | grep -v "asiclib"`
 
 # Retrieve all filelists recursively
 FILELISTS="`retrieve_filelist ${FILELIST}` ${FILELIST}"
+
+###
+echo "${MACRO}: Copy hdl files"
 
 # copy vhdl files
 for vhdl in ${VHDL_FILES}; do
@@ -104,14 +125,27 @@ DEPS=""
 for verilog in ${VERILOG_FILES}; do
   verilog_file=`echo ${verilog} | sed 's#ASIC/##'`
   cp --parents ${verilog_file} ${DEST}
-  deps=`get_deps ${verilog}`
+  deps=`get_deps ${verilog} "${INCDIRS}"`
   for d in ${deps}; do
-    if [ ! -f ${d} ]; then
-      for i in ${INCDIRS}; do
-        DEPS="${DEPS} `find ${i} -name ${d}`"
-      done 
-    else 
-      DEPS="${DEPS} ${d}"
+    # look if current deps has already been processed
+    has_been_processed=0
+    has_been_processed=`echo ${DEPS} | grep "\<${d}\>" -c`
+    if [ ${has_been_processed} -eq 0 ]; then
+      #echo "  search ${d} included in ${verilog}"
+      if [ ! -f ${d} ]; then
+        # Inlude is not absolute path from ASIC/ but rely on incdirs
+        # loop through incdirs to search it
+        for i in ${INCDIRS}; do
+          DEPS="${DEPS} `find ${i} -maxdepth 1 -name ${d}`"
+        done
+      else
+        DEPS="${DEPS} ${d}"
+      fi
+    fi
+    # Check that current included files has been found
+    dep_found=`echo ${DEPS} | grep ${d} -c`
+    if [ $dep_found -eq 0 ]; then
+      echo "[Warning] ${MACRO}: ${d} included in ${verilog} (or through its included files) not found to be copied for import"
     fi
   done
 done
@@ -124,42 +158,69 @@ for d in ${DEPS}; do
 done
 
 # move ASIC/qasara to ASIC/srcv/qasara
-# and create ASIC link at root
+# move ASIC/qasara/IP/ic/shared to ASIC/srcv/common/qasara
 pushd ${DEST} > /dev/null
 if [ -d qasara ]; then
   mkdir -p srcv
   mv qasara srcv/
+  if [ -d srcv/qasara/IP/ic/shared ] && [ ${MACRO} != "ieee_proposed" ]; then
+    mv srcv/qasara/IP/ic/shared srcv/common/qasara
+  fi
 fi
 cd ../
+# remove ASIC link to avoid ln complains that it exists already
+rm -f ASIC
 ln -s ./ ASIC
 popd > /dev/null
 
 # copy filelists
-# update them to the new hierarchy : 
+# update them to the new hierarchy :
 # ASIC/... -> ASIC/macro_name/...
-# ASIC/qasara -> ASIC/macro_name/srcv
+# ASIC/qasara -> ASIC/macro_name/srcv/qasara
+# ASIC/qasara/IP/ic/shared -> ASIC/macro_name/srcv/common/qasara
 # ASIC/srcv/common/asiclib -> ASIC/asiclib/srcv/common/asiclib
+# ASIC/memories -> ASIC/asiclib/memories
 # Create new variable GIT_FILELISTS which contains the list of filelists in the
 # new hierarchy starting at ASIC/macro_name
 mkdir -p `dirname ${DEST}/${FILELIST}`
-#sed "s#ASIC/qasara/#ASIC/srcv/qasara/#" ${FILELIST} | sed "s#ASIC/#ASIC/${MACRO}/#" > ${DEST}/${FILELIST}
-#GIT_FILELISTS=${FILELIST}
+# Create sed script to apply changes to filelists
+SED_UPDATE_FILELISTS=${DEST}/${MACRO}_update_filelists.sed
+echo "#!/bin/sed -f" > ${SED_UPDATE_FILELISTS}
+echo "s@ASIC/\+qasara/\+@ASIC/srcv/qasara/@" >> ${SED_UPDATE_FILELISTS}
+if [ ${MACRO} != "ieee_proposed" ]; then
+  echo "s@ASIC/\+srcv/\+qasara/\+IP/\+ic/\+shared/@ASIC/srcv/common/qasara/@" >> ${SED_UPDATE_FILELISTS}
+fi
+echo "s@ASIC/\+@ASIC/${MACRO}/@" >> ${SED_UPDATE_FILELISTS}
+echo "s@ASIC/\+${MACRO}/\+srcv/\+common/\+asiclib/\+@ASIC/asiclib/srcv/common/asiclib/@" >> ${SED_UPDATE_FILELISTS}
+echo "s@ASIC/\+${MACRO}/\+memories@ASIC/asiclib/memories@" >> ${SED_UPDATE_FILELISTS}
+chmod +x ${SED_UPDATE_FILELISTS}
 for fl in ${FILELISTS}; do
   # remove ASIC prefix
   fl_file=`echo $fl | sed 's#ASIC/##'`
-  # Update destination filelist to the new hierarchy (ASIC/qasara -> ASIC/srcv/qasara)
-  fl_dest_file=`echo ${fl_file} | sed "s#^qasara#srcv/qasara#"`
+  # Update destination filelist to the new hierarchy 
+  # ASIC/qasara -> ASIC/srcv/qasara
+  # ASIC/qasara/IP/ic/shared -> ASIC/srcv/common/qasara
+  fl_dest_file=`echo ${fl_file} | \
+  sed "s#^qasara#srcv/qasara#" | \
+  sed "s#^srcv/qasara/IP/ic/shared#srcv/common/qasara#"`
   GIT_FILELISTS="${GIT_FILELISTS} ${fl_dest_file}"
   # Create the folder of the filelist if it does not exist
   mkdir -p `dirname ${DEST}/${fl_dest_file}`
+  #copy filelist and apply sed script
+  cp ${fl_file} ${DEST}/${fl_dest_file}
+  ${SED_UPDATE_FILELISTS} -i ${DEST}/${fl_dest_file}
   # Create new filelist
-  sed "s#ASIC/\+qasara/\+#ASIC/srcv/qasara/#" ${fl_file} | \
-  sed "s#ASIC/\+#ASIC/${MACRO}/#" | \
-  sed "s#ASIC/\+${MACRO}/\+srcv/\+common/\+asiclib/\+#ASIC/asiclib/srcv/common/asiclib/#" > ${DEST}/${fl_dest_file}
+  #sed "s#ASIC/\+qasara/\+#ASIC/srcv/qasara/#" ${fl_file} | \
+  #sed "s#ASIC/\+srcv/\+qasara/\+IP/\+ic/\+shared/#ASIC/srcv/common/qasara/#" |\
+  #sed "s#ASIC/\+#ASIC/${MACRO}/#" | \
+  #sed "s#ASIC/\+${MACRO}/\+srcv/\+common/\+asiclib/\+#ASIC/asiclib/srcv/common/asiclib/#" | \
+  #sed "s#ASIC/\+${MACRO}/\+memories#ASIC/asiclib/memories#" \
+  #> ${DEST}/${fl_dest_file}
 done
 
 
 ### Start processing macro files ###
+echo "${MACRO}: Patch hdl files"
 pushd ${DEST} > /dev/null
 
 # Creates sed script which will remember the files names changes
@@ -201,13 +262,14 @@ for cf in ${COMMON_FILES}; do
   fi
 
   # process for vhdl files
+  # Search & replace is insensitive ( i flag on sed : s/toto/titi/i)
   if [ `echo ${cf} | grep -c ".vhd$"` -eq 1 ]; then
     # get entity name
     entities_names=`grep "^\s*\<entity\>" ${cf} | sed 's/^\s*\<entity\>\s\+\(\w\+\)\s*.*$/\1/'`
     for e in ${entities_names}; do
       macro_entity_name="${MACRO}_${e}"
       # record entity renaming
-      echo "s/\\<${e}\\>/${macro_entity_name}/" >> ${SED_RENAME_MODULES}
+      echo "s/\\<${e}\\>/${macro_entity_name}/i" >> ${SED_RENAME_MODULES}
     done
 
     # get package if any
@@ -215,7 +277,7 @@ for cf in ${COMMON_FILES}; do
     for p in ${packages_names}; do
       macro_package_name="${MACRO}_${p}"
       # record package renaming
-      echo "s/\\<${p}\\>/${macro_package_name}/" >> ${SED_RENAME_MODULES}
+      echo "s/\\<${p}\\>/${macro_package_name}/i" >> ${SED_RENAME_MODULES}
     done
   fi
 
@@ -225,21 +287,21 @@ for cf in ${COMMON_FILES}; do
   # check if the common file is a file_list
   if [ `echo ${GIT_FILELISTS} | grep -c ${cf}` -ne 0 ]; then
     # then update the GIT_FILELISTS var with the new common file list name
-    GIT_FILELISTS=`echo ${GIT_FILELISTS} | sed "s/${cf_file}/${dest_cf_file}/"`
+    GIT_FILELISTS=`echo ${GIT_FILELISTS} | sed "s/${cf_file}/${dest_cf_file}/g"`
   fi
 done
 
 # update filelists with new common files name
 for fl in ${GIT_FILELISTS};do
-  ${SED_RENAME_FILES} -i.bak ${fl}
+  ${SED_RENAME_FILES} ${fl} -i #-i.bak
   # diff --brief  ${fl} ${fl}.bak
 done
 
 # update HDL source with new modules, entities, packages names.
 for f in `find . -name "*.v" -o -name "*.vhd" -type f`; do
-  ${SED_RENAME_MODULES} -i.bak ${f}
+  ${SED_RENAME_MODULES} ${f} -i #-i.bak
   # also apply the filenames changes script for "include"
-  ${SED_RENAME_FILES} -i.bak2 ${f}
+  ${SED_RENAME_FILES} ${f} -i #-i.bak2
   #diff --brief ${f} ${f}.bak
 done
 
@@ -248,28 +310,46 @@ cd ../
 GIT_HDL_FILES=`${FILEPP} -e -kc "//#" ${MACRO}/${FILELIST} | grep -v "^//" | sed '/^$/d' | grep -v "incdir" | sort | uniq`
 for f in ${GIT_HDL_FILES};do
   if [ ! -f ${f} ]; then
-    echo "oops, $f does not exist"
+    echo "[Warning] ${MACRO} $f is in filelist but does not exist or has not been copied"
   fi
 done
 
 MACRO_FILELIST_PARSED=${MACRO}_file_list_parsed
 GIT_HDL_FILES=`${FILEPP} -e -kc "//#" ${MACRO}/${FILELIST} | ${PARSE_FILE_LIST}`
 
-mkdir -p INCA_libs/worklib
-mkdir -p INCA_libs/ieee_proposed
-HDLVAR=INCA_libs/hdl.var
-echo "define work worklib" > ${HDLVAR}
+COMPILE_SCRIPT="${MACRO}/simv/${MACRO}_compile.sh"
+mkdir -p `dirname ${COMPILE_SCRIPT}`
+echo "#!/bin/bash -e" > ${COMPILE_SCRIPT}
+echo "if [ ! -h ASIC ]; then" >> ${COMPILE_SCRIPT}
+echo "  ln -s ../.. ASIC" >> ${COMPILE_SCRIPT}
+echo "fi" >> ${COMPILE_SCRIPT}
 
-CDSLIB=INCA_libs/cds.lib
+# INCA_libs
+echo "mkdir -p ASIC/${MACRO}/simv/INCA_libs/worklib" >> ${COMPILE_SCRIPT}
+
+# hdl.var
+HDLVAR=ASIC/${MACRO}/simv/INCA_libs/hdl.var
+echo "echo define work worklib > ${HDLVAR}" >> ${COMPILE_SCRIPT}
+
+#cds.lib
+CDSLIB=ASIC/${MACRO}/simv/INCA_libs/cds.lib
 IUS_HOME=`${GETVERSION}  --iushome`
-echo "include ${IUS_HOME}/tools/inca/files/cds.lib" > ${CDSLIB}
-echo "define worklib ./worklib" >> ${CDSLIB}
-echo "define ieee_proposed ieee_proposed" >> ${CDSLIB}
+echo "echo include ${IUS_HOME}/tools/inca/files/cds.lib > ${CDSLIB}" >> ${COMPILE_SCRIPT}
+echo "echo define worklib ./worklib >> ${CDSLIB}" >> ${COMPILE_SCRIPT}
+echo "echo define ieee_proposed ieee_proposed >> ${CDSLIB}" >> ${COMPILE_SCRIPT}
 
 if [ ${MACRO} = "ieee_proposed" ]; then
-  echo ncvhdl -v93 -cdslib ${CDSLIB} -hdlvar ${HDLVAR} -ASSERT  -NOPRAGMAWARN -nowarn IGNPRG -work ieee_proposed ${GIT_HDL_FILES}
+  echo "mkdir -p ASIC/${MACRO}/simv/INCA_libs/ieee_proposed" >> ${COMPILE_SCRIPT}
+  echo "ncvhdl -v93 -cdslib ${CDSLIB} -hdlvar ${HDLVAR} -ASSERT  -NOPRAGMAWARN -nowarn IGNPRG -work ieee_proposed ${GIT_HDL_FILES}" >> ${COMPILE_SCRIPT}
+else
+
+  echo "if [ ! -h INCA_libs/ieee_proposed ]; then" >> ${COMPILE_SCRIPT}
+  echo "  ln -s ../ASIC/ieee_proposed/simv/INCA_libs/ieee_proposed INCA_libs/ieee_proposed" >> ${COMPILE_SCRIPT}
+  echo "fi" >> ${COMPILE_SCRIPT}
+
+  echo "ASIC/ieee_proposed/simv/ieee_proposed_compile.sh" >> ${COMPILE_SCRIPT}
+  echo "${FILEPP} -e -kc \"//#\" ASIC/${MACRO}/${FILELIST} > ${MACRO_FILELIST_PARSED} " >> ${COMPILE_SCRIPT}
+  echo "irun -f ${MACRO_FILELIST_PARSED} -sv -v93 -cdslib INCA_libs/cds.lib -hdlvar INCA_libs/hdl.var -define NEWCOMPIL -top ${TOP} -timescale 1ns/1ps" >> ${COMPILE_SCRIPT}
 fi
 
-echo "${FILEPP} -e -kc \"//#\" ${MACRO}/${FILELIST} > ${MACRO_FILELIST_PARSED} "
-echo "irun -f ${MACRO_FILELIST_PARSED} -sv -v93 -cdslib INCA_libs/cds.lib -hdlvar INCA_libs/hdl.var"
-
+chmod +x ${COMPILE_SCRIPT}
