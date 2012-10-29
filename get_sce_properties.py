ffe4d7700d4d33ba54564d35ecf10bc42d6e1b82
Nicolas Vincent <nvincent@sequans.com>
2012-10-29 09:40:20 +0100

    change symbolic link to hard link
    get_sce_properties.py was a soft link to outside of git repo

--- get_sce_properties.py
+++ get_sce_properties.py
@@ -1,1 +1,70 @@
-/home/nvincent/work/tools_env/sim/get_sce_properties.py
+#!/usr/bin/env python
+
+from xml.dom import minidom
+import os
+import re
+import sys
+
+#################
+### FUNCTIONS ###
+#################
+
+# Brief help
+def print_help():
+    print "Usage : " + sys.argv[0] + " </path/to/tb_module>"
+    print "This scripts outputs for each scenario all the properties defined in envConfig.lte"
+    print "The output is printed on stdout as a CSV (semi-colon separators) file."
+
+
+# Retrieve list of properties for a given envConfig file
+def getProperties(envConfig):
+    # check if the file exists
+    if os.path.isfile(envConfig):
+        xmldoc = minidom.parse(envConfig)
+        coverage_list = xmldoc.getElementsByTagName("Coverage")
+        coverage = coverage_list[0]
+
+        properties = coverage.attributes.keys()
+    else:
+        properties = []
+
+    return properties
+
+
+# Retrieve list of scenario for a given folder, scenario folder should start with "sce"
+def getScenarii(folder):
+    ls = os.listdir(folder)
+    scenarii = []
+    for f in ls:
+        # Check for sce at begining of folder name
+        if re.match("sce", f):
+            scenarii.append(f)
+    scenarii.sort()
+    return scenarii
+
+
+
+############
+### MAIN ###
+############
+
+# retrieve module simulation folder
+module = sys.argv[1]
+
+if not(os.path.isdir(module)):
+    print_help()
+    exit(255)
+
+
+scenarii = getScenarii(module)
+for sce in scenarii:
+    envConfig = module + "/" + sce + "/envConfig.lte"
+    props = getProperties(envConfig)
+    # create string for properties
+    props_str = ""
+    for p in props:
+        props_str = props_str + p + " "
+
+    # display
+    print sce + ";;" + props_str + ";"
+
