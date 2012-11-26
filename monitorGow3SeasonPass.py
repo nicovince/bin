f63752dba4799b6ea66a76bdf6499373bb3c3e4a
Nicolas Vincent <nico.vince@gmail.com>
2012-11-26 15:29:39 +0100

    monitor uses htmlparser python module
    * uses proper html parser to retrieve price of content

--- monitorGow3SeasonPass.py
+++ monitorGow3SeasonPass.py
@@ -4,6 +4,28 @@ import os, sys, re
 import urllib2
 import smtplib
 from email.mime.text import MIMEText
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
+
+class XboxMarketHtmlParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.match = False
+        self.price = 0
+    def handle_starttag(self, tag, attrs):
+        if tag == "span":
+            for attr in attrs:
+                if (attr[0] == "class" and attr[1] == "MSPoints SilverPrice ProductPrice"):
+                    self.match = True
+    def handle_data(self, data):
+        if self.match:
+            self.price = data.replace(',','')
+            self.match = False
+    def getPrice(self):
+        return self.price
+
+
+
 
 # Send mail, subject is prefixed with "[Hella Nzb] "
 def sendMail(subject, content):
@@ -45,6 +67,10 @@ url = "http://marketplace.xbox.com/en-US/Product/Gears-of-War-3-Season-Pass/b38b
 data = getPage(url)
 price = getPrice(data)
 currentPrice="2400"
+parser = XboxMarketHtmlParser()
+parser.feed(data)
+price2 = parser.getPrice()
+print price2
 for p in price:
     if p < currentPrice:
         sendMail("Gow3 Season Pass","Price of Season pass is " + p + "\n" + str(price))
