048f2c581abee15f6a77ff84873827dc4f7ac362
Nicolas Vincent <nico.vince@gmail.com>
2012-12-09 21:40:22 +0100

    Fix path to dispatch_usenet.py script
    * uses a variable to store path of script

--- testing/testing_dispatch.sh
+++ testing/testing_dispatch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #setup tests
 
+DISPATCH_USENET="../dispatch_usenet.py"
 videosFolder="${PWD}/videosFolder"
 rm -Rf nzb_*
 rm -Rf dummy/
@@ -12,49 +13,49 @@ mkdir nzb_nomatch
 touch nzb_nomatch/bambi.avi
 
 echo "--- Should move video_dummy.mp4 to ${videosFolder}/dummy ---"
-../bin.dev/dispatch_usenet.py --type "SUCCESS" \
-                              --archiveName "nzb_dummy" \
-                              --destDir "/home/admin/dev/testing/nzb_dummy" \
-                              --elapsedTime "2m 43s" \
-                              --parMessage "" \
-                              --videosFolder "${videosFolder}" \
-                              --verbose
+$DISPATCH_USENET --type "SUCCESS" \
+                 --archiveName "nzb_dummy" \
+                 --destDir "/home/admin/dev/testing/nzb_dummy" \
+                 --elapsedTime "2m 43s" \
+                 --parMessage "" \
+                 --videosFolder "${videosFolder}" \
+                 --verbose
 
 echo "--- Should find nothing ---"
-../bin.dev/dispatch_usenet.py --type "SUCCESS" \
-                              --archiveName "nzb_dummy" \
-                              --destDir "/home/admin/dev/testing/nzb_dummy" \
-                              --elapsedTime "2m 43s" \
-                              --parMessage "" \
-                              --videosFolder "${videosFolder}" \
-                              --verbose
+$DISPATCH_USENET --type "SUCCESS" \
+                 --archiveName "nzb_dummy" \
+                 --destDir "/home/admin/dev/testing/nzb_dummy" \
+                 --elapsedTime "2m 43s" \
+                 --parMessage "" \
+                 --videosFolder "${videosFolder}" \
+                 --verbose
 
 echo "--- Should move nothing ---"
 touch nzb_dummy/video_dummy.mp4
-../bin.dev/dispatch_usenet.py --type "SUCCESS" \
-                              --archiveName "nzb_dummy" \
-                              --destDir "/home/admin/dev/testing/nzb_dummy" \
-                              --elapsedTime "2m 43s" \
-                              --parMessage "" \
-                              --videosFolder "${videosFolder}" \
-                              --verbose
+$DISPATCH_USENET --type "SUCCESS" \
+                 --archiveName "nzb_dummy" \
+                 --destDir "/home/admin/dev/testing/nzb_dummy" \
+                 --elapsedTime "2m 43s" \
+                 --parMessage "" \
+                 --videosFolder "${videosFolder}" \
+                 --verbose
 
 echo "--- Should move sample_dummy.mp4 to ${videosFolder}/dummy ---"
 touch nzb_dummy/video_dummy.mp4
 touch nzb_dummy/sample_dummy.mp4
-../bin.dev/dispatch_usenet.py --type "SUCCESS" \
-                              --archiveName "nzb_dummy" \
-                              --destDir "/home/admin/dev/testing/nzb_dummy" \
-                              --elapsedTime "2m 43s" \
-                              --parMessage "" \
-                              --videosFolder "${videosFolder}" \
-                              --verbose
+$DISPATCH_USENET --type "SUCCESS" \
+                 --archiveName "nzb_dummy" \
+                 --destDir "/home/admin/dev/testing/nzb_dummy" \
+                 --elapsedTime "2m 43s" \
+                 --parMessage "" \
+                 --videosFolder "${videosFolder}" \
+                 --verbose
 
 echo "--- should not move anything ---"
-../bin.dev/dispatch_usenet.py --type "SUCCESS" \
-                              --archiveName "nzb_nomatch" \
-                              --destDir "/home/admin/dev/testing/nzb_nomatch" \
-                              --elapsedTime "2m 43s" \
-                              --parMessage "" \
-                              --videosFolder "${videosFolder}" \
-                              --verbose
+$DISPATCH_USENET --type "SUCCESS" \
+                 --archiveName "nzb_nomatch" \
+                 --destDir "/home/admin/dev/testing/nzb_nomatch" \
+                 --elapsedTime "2m 43s" \
+                 --parMessage "" \
+                 --videosFolder "${videosFolder}" \
+                 --verbose
