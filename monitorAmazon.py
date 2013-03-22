73a453f2d781945a3b4dc656405dabfd5ccaf1e8
Nicolas Vincent <nico.vince@gmail.com>
2013-03-22 19:29:26 +0100

    Adds title parsing
    * adds comment
    * adds title parsing + method to retrieve title

--- monitorAmazon.py
+++ monitorAmazon.py
@@ -21,42 +21,57 @@ class AmazonHtmlParser(HTMLParser):
         HTMLParser.__init__(self)
         self.matchActualPriceValue = False
         self.matchPriceLarge = False
+        self.matchTitle = False
         self.price = list()
     def handle_starttag(self, tag, attrs):
-        #print tag
-        #print attrs
         if tag == "span":
             for attr in attrs:
                 if (attr[0] == "id" and attr[1] == "actualPriceValue"):
+                    # Price parsing
                     self.matchActualPriceValue = True
+                elif (attr[0] == "id" and attr[1] == "btAsinTitle"):
+                    # title parsing
+                    self.matchTitle = True
         elif tag == "b":
             for attr in attrs:
                 if (self.matchActualPriceValue and attr[0] == "class" and attr[1] == "priceLarge"):
+                    # found all tags/attributes for price
                     self.matchPriceLarge = True
 
     def handle_data(self, data):
         if (self.matchActualPriceValue and self.matchPriceLarge):
-            self.price.append(data.replace(',','.').replace("EUR ",""))
+            # Price
+            # Remove EUR prefix
+            strPrice = data.replace(',','.').replace("EUR ","")
+            # cast to float
+            self.price.append(float(strPrice.encode('utf-8','ignore')))
             self.matchActualPriceValue = False
             self.matchPriceLarge = False
+        elif (self.matchTitle):
+            # Title
+            self.title = data
+            self.matchTitle = False
+
     def getPrice(self):
         return self.price
+    def getTitle(self):
+        return self.title
+        
 
-url ="http://www.amazon.fr/Take-2-BioShock-Infinite/dp/B004Z6A9GK/ref=pd_sim_vg_4"
 url="http://www.amazon.fr/gp/product/B0088O0JQK/ref=oh_details_o00_s00_i00?ie=UTF8&psc=1"
+url ="http://www.amazon.fr/Take-2-BioShock-Infinite/dp/B004Z6A9GK/ref=pd_sim_vg_4"
 
 
-#data = getPage(url)
+data = getPage(url)
 fd = open('amazon.html','r')
-data = fd.read()
+#data = fd.read()
+fd.close()
 #dataclean = data.replace("è","e")
 dataclean = data.decode('utf-8','ignore')
-fd_clean = open('clean.html','w')
-fd_clean.write(dataclean)
-fd_clean.close()
 parser = AmazonHtmlParser()
 parser.feed(dataclean)
-price2 = parser.getPrice()
-print price2
-for price in price2:
+priceList = parser.getPrice()
+print parser.getTitle()
+print priceList
+for price in priceList:
     print price
