f6e60710ca2d23e09b41c96eabd3c2ddd64dae52
Nicolas Vincent <nico.vince@gmail.com>
2013-04-01 13:42:37 +0200

    getInfos -> get_infos

--- monitorAmazon.py
+++ monitorAmazon.py
@@ -12,7 +12,7 @@ url="http://www.amazon.fr/gp/product/B0088O0JQK/ref=oh_details_o00_s00_i00?ie=UT
 url ="http://www.amazon.fr/Take-2-BioShock-Infinite/dp/B004Z6A9GK/ref=pd_sim_vg_4"
 url = "http://www.amazon.fr/Lego-le-Seigneur-des-Anneaux/dp/B0088O0KI2/ref=sr_1_1_title_1?s=videogames&ie=UTF8&qid=1364062396&sr=1-1"
 
-def getInfos(url):
+def get_infos(url):
     web_page = get_page(url)
     web_page_clean = web_page.decode('utf-8', 'ignore')
     parser = AmazonHtmlParser()
@@ -23,5 +23,5 @@ def getInfos(url):
 
 
 if __name__ == "__main__":
-    [price, item] = getInfos(url)
+    [price, item] = get_infos(url)
     print item + " costs " + str(price) + " euros"
