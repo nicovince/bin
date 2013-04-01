2bc59152fffce9f1fc7aaf4127df63cee7816230
Nicolas Vincent <nico.vince@gmail.com>
2013-04-01 13:37:31 +0200

    Move code into function called by main
    * getInfos returns object name and price

--- monitorAmazon.py
+++ monitorAmazon.py
@@ -12,18 +12,16 @@ url="http://www.amazon.fr/gp/product/B0088O0JQK/ref=oh_details_o00_s00_i00?ie=UT
 url ="http://www.amazon.fr/Take-2-BioShock-Infinite/dp/B004Z6A9GK/ref=pd_sim_vg_4"
 url = "http://www.amazon.fr/Lego-le-Seigneur-des-Anneaux/dp/B0088O0KI2/ref=sr_1_1_title_1?s=videogames&ie=UTF8&qid=1364062396&sr=1-1"
 
+def getInfos(url):
+    web_page = getPage(url)
+    web_page_clean = web_page.decode('utf-8', 'ignore')
+    parser = AmazonHtmlParser()
+    parser.feed(web_page_clean)
+    priceList = parser.getPrice()
+    return [priceList[0], parser.getTitle()]
+
+
 
 if __name__ == "__main__":
-    data = getPage(url)
-    fd = open('amazon.html','r')
-    #data = fd.read()
-    fd.close()
-    #dataclean = data.replace("è","e")
-    dataclean = data.decode('utf-8','ignore')
-    parser = AmazonHtmlParser()
-    parser.feed(dataclean)
-    priceList = parser.getPrice()
-    print parser.getTitle()
-    print priceList
-    for price in priceList:
-        print price
+    [price, item] = getInfos(url)
+    print item + " costs " + str(price) + " euros"
