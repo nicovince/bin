541fca93256d3b1cb9eb440a2d514b949df89955
Nicolas Vincent <nico.vince@gmail.com>
2014-07-21 11:36:01 +0200

    update checklog to check between private and server

--- checkGitLog.sh
+++ checkGitLog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 SHA=$1
-message=`git log --format="%s %b" $SHA -n 1`
-/delivery/lib/latest5-2/scripts/checkLogKeyword.py "git" "${message}"
+SHAS=`git log --format="%H" origin/private/nvincent/main..private/nvincent/main`
+for s in $SHAS; do
+  message=`git log --format="%s %b" $s -n 1`
+  echo "check $s"
+  /delivery/lib/latest5-2/scripts/checkLogKeyword.py "git" "${message}"
+done
