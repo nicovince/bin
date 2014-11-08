457b61ddba7cfc076b22d4ab32b6172cd629b8f0
Nicolas Vincent <nico.vince@gmail.com>
2014-11-08 14:36:25 +0100

    minecraft server launcher
    - create screen session in detached mode and launch minecraft
    - screen created if no mcj screen session exists
    - server launched if not started yet

--- mcjwrap.sh
+++ mcjwrap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-date
+SCREEN_NAME="mcj"
+SERVER_TAB_NAME="server"
 NB=`screen -ls | grep -c mcj`
 # Does an existing screen session exist
-if [ $NB -eq 1 ]; then
-  screen -RD mcj
-else
-  screen -S mcj -c ~/configrc/cosmopolitan.mcj.screenrc
+if [ $NB -eq 0 ]; then
+  # No, let's create one in detached mode
+  echo "Creating Screen session to host minecraft server"
+  screen -d -m -S $SCREEN_NAME -c ~/configrc/cosmopolitan.mcj.screenrc
 fi
 
-date
-
+# Is minecraft server already running
+MC_RUNNING=`ps aux | grep minecraft_server | grep -c java`
+if [ $MC_RUNNING -ne 1 ]; then
+  # No, let's create a new one
+  echo screen -S $SCREEN_NAME -p $SERVER_TAB_NAME -X stuff "mcj$(printf \\r)"
+else
+  echo "Minecraft server already running"
+fi
