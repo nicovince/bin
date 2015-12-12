5dbe42f98f0ed0671dd6fd898e57a5eab57e0534
Nicolas Vincent <nico.vince@gmail.com>
2015-12-12 18:51:07 +0100

    give possibility to launch another world

--- mcjwrap.sh
+++ mcjwrap.sh
@@ -2,6 +2,29 @@
 
 SCREEN_NAME="mcj"
 SERVER_TAB_NAME="server"
+GAME=$1
+# Default launch mcj
+if [ "$GAME" = "" ];
+then
+  GAME="mcj"
+fi
+
+case $GAME in
+  "mcj" )
+    cmd="mcj"
+    ;;
+  "ragecraft" )
+    cmd="cd ~/MinecraftServer/ragecraft_3; ./start.sh"
+    ;;
+  "creatif" )
+    cmd="cd ~/MinecraftServer/Creatif; ./start.sh"
+    ;;
+  *)
+    echo "unknown server"
+    exit 1
+esac
+echo $cmd
+
 NB=`screen -ls | grep -c mcj`
 # Does an existing screen session exist
 if [ $NB -eq 0 ]; then
@@ -15,7 +38,7 @@ MC_RUNNING=`ps aux | grep minecraft_server | grep -c java`
 if [ $MC_RUNNING -ne 1 ]; then
   # No, let's create a new one
   echo "Start Minecraft Server"
-  screen -S $SCREEN_NAME -p $SERVER_TAB_NAME -X stuff "mcj$(printf \\r)"
+  screen -S $SCREEN_NAME -p $SERVER_TAB_NAME -X stuff "$cmd$(printf \\r)"
 else
   echo "Minecraft server already running"
 fi
