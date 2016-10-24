5328dbae4bd58eddeaccdb81ad093a177b4ab9fd
Nicolas VINCENT <nico.vince@gmail.com>
2016-10-24 18:59:13 +0200

    update timelapse script to generate 10fps

--- timelapse_botanic.sh
+++ timelapse_botanic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3 -vf scale=650:533 -mf type=png:fps=1 mf://@list.txt -o time-lapse.avi
+mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3 -vf scale=650:533 -mf type=jpg:fps=10 mf://@list.txt -o time-lapse.avi
