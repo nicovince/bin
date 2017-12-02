d9af262ab2edabcb6a38add17d8c834e0cbf204c
Nicolas VINCENT <nico.vince@gmail.com>
2017-12-02 13:03:26 +0100

    change filename, no conversion/link

--- snap_botanic.sh
+++ snap_botanic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEST=/home/pi/Documents/botanic
-FILE_SUFFIX=tomatoes_`date +%F-%H-%R`
+FILE_SUFFIX=tomatoes_`date +%F-%R`
 FILENAME_ORIG=$DEST/${FILE_SUFFIX}.jpg
 FILENAME_CROPPED=$DEST/${FILE_SUFFIX}_cropped.jpg
 
@@ -9,11 +9,10 @@ DST_LINK=/var/www/html/latest.png
 DST_LINK_ORIG=/var/www/html/latest_orig.png
 
 fswebcam --no-banner -r 1280x720 --skip 3 $FILENAME_ORIG
-convert $FILENAME_ORIG -crop 650x553+263+0 $FILENAME_CROPPED
-#cp $FILENAME_ORIG $FILENAME_CROPPED
+#convert $FILENAME_ORIG -crop 650x553+263+0 $FILENAME_CROPPED
 
 rm -f $DST_LINK
 rm -f $DST_LINK_ORIG
-ln -s $FILENAME_CROPPED $DST_LINK
+#ln -s $FILENAME_CROPPED $DST_LINK
 ln -s $FILENAME_ORIG $DST_LINK_ORIG
 
