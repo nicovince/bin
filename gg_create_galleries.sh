19d4ff7203888fb91f60a08a3c0356316fb7047e
Nicolas VINCENT <nico.vince@gmail.com>
2019-04-04 15:49:57 +0200

    Fix gallery generation
    - path to galery does not have to be . anymore, can be anything.
    - if directory containing root script is not in path it will work anyway
      as long as others scripts are in same dir

--- gg_create_galleries.sh
+++ gg_create_galleries.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+BIN_DIR=$(dirname $(realpath $0))
 GALLERY_ROOT=$1
 THUMBS_DIR=${GALLERY_ROOT}/.thumbs
 
 # Generate thumbnails for all images found under gallery's root.
 echo "Create thumbnails"
-gg_create_thumbnails.sh ${GALLERY_ROOT}
+${BIN_DIR}/gg_create_thumbnails.sh ${GALLERY_ROOT}
 
 # Create htmls pages for each folder
-for d in $(find . -path '*/\.thumbs*' -prune -o -type d  -print); do
+for d in $(find ${GALLERY_ROOT} -path '*/\.thumbs*' -prune -o -type d  -print); do
   echo "Create $d Gallery"
-  gg_create_html.py ${d} ${THUMBS_DIR}
+  ${BIN_DIR}/gg_create_html.py ${d} ${THUMBS_DIR}
 done
--- gg_create_thumbnails.sh
+++ gg_create_thumbnails.sh
@@ -8,33 +8,38 @@ IMG_EXT="jpg png JPG"
 function gen_img_list()
 {
   local img_dir=$1
-  local thumbs_dir=$2
+  local thumbs_dir=$(realpath --relative-to=${img_dir} $2)
+  pushd ${img_dir} > /dev/null
   for ext in ${IMG_EXT}; do
     # List image found under hierarchy, excluding the ones present in thumbs dir
-    for img in $(find ${img_dir} -path ${thumbs_dir} -prune -o -name "*.${ext}" -print); do
+    for img in $(find . -path ./${thumbs_dir} -prune -o -name "*.${ext}" -print); do
       # report image if it is not present in thumbs dir
       if [ ! -f ${thumbs_dir}/${img} ]; then
         echo ${img}
       fi
     done
   done
+  popd > /dev/null
 }
 
 function create_thumbs()
 {
-  local img_list=$1
-  local thumbs_dir=$2
+  local img_dir=$1
+  local img_list=$2
+  local thumbs_dir=$3
+  pushd $img_dir > /dev/null
   gm mogrify -output-directory ${thumbs_dir} -create-directories -resize 320x200 @${img_list}
+  popd > /dev/null
 }
 
-IMAGE_DIR=$1
+IMAGE_DIR=$(realpath $1)
 THUMBS_DIR=${IMAGE_DIR}/.thumbs
 
 # Get list of thumbnails to create, and store them in a file
 IMG_LIST=$(mktemp)
 gen_img_list ${IMAGE_DIR} ${THUMBS_DIR} > ${IMG_LIST}
 # Create thumbnails to thumbs directory
-create_thumbs ${IMG_LIST} ${THUMBS_DIR}
+create_thumbs ${IMAGE_DIR} ${IMG_LIST} ${THUMBS_DIR}
 
 # Remove file list
 rm -f ${IMG_LIST}
