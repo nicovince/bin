6c074c282cf3c022ca70693cbc7bbc9ff3aa5585
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-03-27 15:28:33 +0100

    adds .tmux.conf

--- dotfiles_bootstrap.py
+++ dotfiles_bootstrap.py
@@ -172,6 +172,7 @@ class BootStrap:
                      '.bash_colors',
                      '.inputrc',
                      '.screenrc',
+                     '.tmux.conf',
                      '.gitignore_global',
                      '.gitconfig',
                      ]
