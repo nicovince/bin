4bfe4d6db0b893ffcfeda69824588ddbede5a592
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2022-05-24 14:28:58 +0200

    Make dhcp flush lease script portable
    Interface name can be specified on command line.

--- flush_dhcp_leases.sh
+++ flush_dhcp_leases.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cat /var/lib/NetworkManager/dnsmasq-eno1.leases | cut -d ' ' -f"3 2" | awk '{print $2 " " $1}' | while read line; do
+NET_IF=${1:-eno1}
+DNSMASK_FILE="/var/lib/NetworkManager/dnsmasq-${NET_IF}.leases"
+
+if [ ! -f ${DNSMASK_FILE} ]; then
+    echo "${DNSMASK_FILE} does not exist, specify interface name."
+    exit 1
+fi
+cat ${DNSMASK_FILE} | cut -d ' ' -f"3 2" | awk '{print $2 " " $1}' | while read line; do
     ip=$(echo "${line}" | cut -d' ' -f 1)
     mac=$(echo "${line}" | cut -d' ' -f 2)
-    dhcp_release eno1 $ip $mac "*"
+    dhcp_release ${NET_IF} $ip $mac "*"
 done
