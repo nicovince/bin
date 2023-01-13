418e2ba13bc438874ec56698d93f8f07e9010abe
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2023-01-13 12:41:56 +0100

    smb mount: option to select smb drive
    smb drive devices and mount point are stored in associative array.
    cli option can be used to specify specific drives to mount.

--- smb_mgmt.sh
+++ smb_mgmt.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+declare -A SMB_MNT_INFOS
+SMB_MNT_INFOS[U]="//SV-VLB-0001/users$/nicolas.vincent"
+SMB_MNT_INFOS[S]="//SV-VLB-0001/services"
+SMB_MNT_INFOS[R]="//SV-VLB-0002/ref"
+SMB_MNT_INFOS[X]="//SV-VLB-0002/echange"
 
 SMB_CRED="$HOME/.smbcred"
 MNT_OPTS="_netdev,credentials=${SMB_CRED},dir_mode=0555,file_mode=0444,uid=500,gid=500"
 
 MOUNT=0
 UMOUNT=0
+DRIVES=""
+DRIVE_LIST="U S R X"
 while [ $# -gt 0 ]; do
     key="$1"
     case ${key} in
@@ -12,23 +20,47 @@ while [ $# -gt 0 ]; do
             MOUNT=1
             shift
             ;;
+        -u)
+            DRIVES="${DRIVES} U"
+            shift
+            ;;
+        -s)
+            DRIVES="${DRIVES} S"
+            shift
+            ;;
+        -r)
+            DRIVES="${DRIVES} R"
+            shift
+            ;;
+        -x)
+            DRIVES="${DRIVES} X"
+            shift
+            ;;
         umount)
             UMOUNT=1
             shift
             ;;
+        *)
+            echo "Unknown option ${key}"
+            exit 1
+            ;;
     esac
 done
 
+if [ -z "${DRIVES}" ]; then
+    DRIVES="U S R X"
+fi
+
 if [ ${MOUNT} -eq 1 ]; then
-    sudo mount.cifs //SV-VLB-0001/users$/nicolas.vincent /media/smb_U/ -o ${MNT_OPTS}
-    sudo mount.cifs //SV-VLB-0001/services /media/smb_S/               -o ${MNT_OPTS}
-    sudo mount.cifs //SV-VLB-0002/ref /media/smb_R/                    -o ${MNT_OPTS}
-    sudo mount.cifs //SV-VLB-0002/echange /media/smb_X/                -o ${MNT_OPTS}
+    for drive in ${DRIVES}; do
+        mnt_point="/media/smb_${drive}"
+        sudo mount.cifs ${SMB_MNT_INFOS[${drive}]} ${mnt_point} -o ${MNT_OPTS}
+    done
 fi
 
 if [ ${UMOUNT} -eq 1 ]; then
-    sudo umount /media/smb_U
-    sudo umount /media/smb_S
-    sudo umount /media/smb_R
-    sudo umount /media/smb_X
+    for drive in ${DRIVES}; do
+        mnt_point="/media/smb_${drive}"
+        sudo umount ${mnt_point}
+    done
 fi
