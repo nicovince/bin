b0f5bbd57c2fba5515cb4443d60c93e11e9c07ef
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2023-02-23 11:11:08 +0100

    dl_gh_artifact: add some comments
    Explain what is beeing done for future readers.

--- dl_gh_action_artifacts.sh
+++ dl_gh_action_artifacts.sh
@@ -5,7 +5,10 @@ CONCLUSION="failure"
 LIMIT=50
 ARTIFACT_NAME="pytest-logs"
 
+# jq -s to force creation of an array of all the items
 FAILED_WF=$(gh run list -L ${LIMIT}  --json conclusion,databaseId,createdAt,displayTitle -w "${WORKFLOW_NAME}" | jq ".[] | select(.conclusion == \"${CONCLUSION}\")" | jq -s ".")
+# use base64 to format each entry of the array in order to iterate properly (space based iteration).
+# Inside the for loop the fields of each items are decoded and parsed.
 for f in $(echo ${FAILED_WF} | jq -r '.[] | @base64'); do
     ts=$(echo ${f} | base64 --decode | jq -r '.createdAt')
     title=$(echo ${f} | base64 --decode | jq -r '.displayTitle')
